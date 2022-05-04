a0fc9a891e4f983697c86a48c5ff7493e3ceae47
Abdollah Zamannoun <abbezz@live.com>
2022-05-04 17:16:43 +0100

    server.js created

---
+++ routes/apiRoutes.js
@@ -0,0 +1,45 @@
+const { reverse } = require("dns")
+
+
+const express = require("express")
+
+const router = express.Router()
+
+const fs =  require("fs")
+
+router.get("/api/notes",(req, res) => {
+    fs.readFile("db/db.json","utf-8",(err,data) => {
+        if(err){
+            console.log(err)}
+            else{
+                return res.json(JSON.parse(data))
+            }
+    })
+})
+
+router.post("/api/notes",(req, res) => {
+    fs.readFile("db/db.json","utf-8",(err, data) => {
+        if(err){
+            console.log(err)
+        }
+        else{
+            const db = JSON.parse(data)
+            const newNote=req.body
+            newNote.id = uuid();
+            db.push(newNote)
+            fs.writeFile("db/db.json",JSON.stringify(db),(err) => {
+                if(err){
+                    console.log(err)
+                }
+                else{
+                    return res.json(db)
+                }
+            })
+        }
+    })
+})
+
+
+
+module.exports = router;
+// module.exports = public;
---
+++ server.js
@@ -0,0 +1,19 @@
+const express =require('express');
+
+const htmlRoutes=require('./routes/htmlRoutes');
+
+const apiRoutes=require('./routes/apiRoutes');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(apiRoutes);
+app.use(htmlRoutes)
+
+
+app.listen(PORT, () => console.log('Server is listening on port ${PORT}'));
